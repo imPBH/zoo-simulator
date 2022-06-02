20474d1562c073a1c1e5a675fb1549e7bab939c4
Alexis Provo <alexis.provo@ynov.com>
2022-06-02 15:46:46 +0200

    Create a tiger

--- main.cpp
+++ main.cpp
@@ -1,37 +1,88 @@
 #include <iostream>
 #include <vector>
 
+#define MEAT 0
+#define SEEDS 1
+
+#define MALE 0
+#define FEMALE 1
+
 using namespace std;
 
 class IAnimal {
     int _age;
-    int _price;
     bool _sex;
     int _food;
-    float _foodByDay;
     int _daysBeforeHunger;
-    int _sexualMaturity;
-    int _pregnancy;
     int _endOfReproduction;
-    float _infantMortality;
     int _lengthOfLife;
     int _observation;
+protected:
+    int _price;
+    float _foodByDay;
+    int _pregnancy;
+    int _sexualMaturity;
+    float _infantMortality;
 public:
-    IAnimal(int age, int price, bool sex, int food, float foodByDay, int daysBeforeHunger, int sexualMaturity,
-            int pregnancy, int endOfReproduction, float infantMortality, int lengthOfLife, int observation);
+    IAnimal(int age, bool sex, int food, int daysBeforeHunger,
+            int endOfReproduction, int lengthOfLife,
+            int observation);
+
+    int Age();
+
+    string Sex();
 };
 
-IAnimal::IAnimal(int age, int price, bool sex, int food, float foodByDay, int daysBeforeHunger,
-                 int sexualMaturity, int pregnancy, int endOfReproduction, float infantMortality, int lengthOfLife,
-                 int observation) : _age(age), _price(price), _sex(sex), _food(food), _foodByDay(foodByDay),
-                                    _daysBeforeHunger(daysBeforeHunger), _sexualMaturity(sexualMaturity),
-                                    _pregnancy(pregnancy),
-                                    _endOfReproduction(endOfReproduction), _infantMortality(infantMortality),
+IAnimal::IAnimal(int age, bool sex, int food, int daysBeforeHunger,
+                 int endOfReproduction, int lengthOfLife,
+                 int observation) : _age(age), _sex(sex), _food(food),
+                                    _daysBeforeHunger(daysBeforeHunger),
+                                    _endOfReproduction(endOfReproduction),
                                     _lengthOfLife(lengthOfLife),
                                     _observation(observation) {
-
 }
 
+int IAnimal::Age() {
+    return _age;
+}
+
+string IAnimal::Sex() {
+    switch (_sex) {
+        case MALE:
+            return "male";
+            break;
+        case FEMALE:
+            return "female";
+            break;
+    }
+}
+
+class Tiger : public IAnimal {
+public:
+    Tiger(int age, bool sex, int observation);
+};
+
+Tiger::Tiger(int age, bool sex, int observation) :
+        IAnimal(age, sex, MEAT, 2, 14, 25, observation) {
+    switch (sex) {
+        case MALE:
+            _foodByDay = 12;
+            _sexualMaturity = 72;
+            _pregnancy = 0;
+            _infantMortality = 0;
+            break;
+        case FEMALE:
+            _foodByDay = 10;
+            _sexualMaturity = 48;
+            _pregnancy = 90;
+            _infantMortality = 0.33;
+            break;
+    }
+}
+
+
 int main() {
+    Tiger *t = new Tiger(0, MALE, 0);
+    cout << "I am " << t->Age() << " months old, and I am a " << t->Sex() << endl;
     return 0;
 }
