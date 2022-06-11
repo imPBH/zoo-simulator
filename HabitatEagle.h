f297ad153e5fbfc496f6ac39c77ca65b780f517c
imPBH <alex.provo31@gmail.com>
2022-06-11 16:50:13 +0200

    Habitat for eagle

---
+++ HabitatEagle.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexis on 11/06/2022.
+//
+
+#ifndef TP_ZOO_CPP_HABITATEAGLE_H
+#define TP_ZOO_CPP_HABITATEAGLE_H
+
+#include "IHabitat.h"
+
+class HabitatEagle : public IHabitat {
+public:
+    HabitatEagle();
+};
+
+
+#endif //TP_ZOO_CPP_HABITATEAGLE_H
