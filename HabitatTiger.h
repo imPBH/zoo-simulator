ec62b0dae41cc9a863834d3265ba91d9062e47fc
imPBH <alex.provo31@gmail.com>
2022-06-11 16:51:47 +0200

    Tiger Habitat

---
+++ HabitatTiger.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexis on 11/06/2022.
+//
+
+#ifndef TP_ZOO_CPP_HABITATTIGER_H
+#define TP_ZOO_CPP_HABITATTIGER_H
+
+#include "IHabitat.h"
+
+class HabitatTiger : public IHabitat {
+public:
+    HabitatTiger();
+};
+
+
+#endif //TP_ZOO_CPP_HABITATTIGER_H
